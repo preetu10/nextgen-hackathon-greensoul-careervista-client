7708952185c66cd751db2bd8e15ed3a420399064
Mahfuja <bajibaji1004@gmail.com>
2025-11-13 03:06:44 +0600

    Navbar, Footer finalized. Profile and Update Profile page initiated.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,23 +1,34 @@
-import { createBrowserRouter } from 'react-router-dom'
-import MainLayout from '../layouts/MainLayout';
-import Login from '../pages/Login';
-
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login";
+import About from "../pages/About";
+import Error from "../../Error";
+import Profile from "../pages/Profile/profile";
+import UpdateProfile from "../pages/Profile/UpdateProfile";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <MainLayout/>,
+    path: "/",
+    element: <MainLayout />,
+    errorElement:<Error></Error>,
     children: [
-     
-         
-        {
-
-            path: '/login',
-          element:<Login/>
-          },
-         
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path:"/user-profile",
+        element:<Profile></Profile>
+      },
+      {
+        path:"/update-profile",
+        element:<UpdateProfile></UpdateProfile>
+      }
     ],
   },
- 
-
 ]);
